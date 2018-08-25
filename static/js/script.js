694d1a1dbd742770c1ffad1bc7a1627ef6a81d4a
evgfilim1 <evgfilim1@yandex.ru>
2018-08-25 22:08:30 +0500

    Add scroll buttons in questions page

--- static/js/script.js
+++ static/js/script.js
@@ -51,4 +51,16 @@ $(document).ready(function () {
 $('a.destructive-confirm').click(destructive_confirm);
 $('form.destructive-confirm').submit(destructive_confirm);
 
+$('a.js-scroll').click(function (ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    const id = $(this).attr('href');
+    const elem = $(id);
+    // const elem = document.querySelector(id);
+    // $('html, body').scrollTop(elem.offset().top - document.querySelector('#main-nav').scrollHeight);
+    $('html, body').animate({
+        scrollTop: elem.offset().top - document.querySelector('#main-nav').scrollHeight
+    }, 500);
+});
+
 $('#noscript').hide();
